624290b3ccc1e412758cc827b70764166aa1e190
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-04-06 12:46:55 -0400

    cloned code

---
+++ script.js
@@ -0,0 +1,27 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+  function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
